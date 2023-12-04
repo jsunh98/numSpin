17c45bd8ef7adbc41533881ce7973e344dfe4253
jsunh98 <jsunh98@gmail.com>
2023-12-03 20:45:12 -0500

    finished adding styling to the meter and coded in app.js for the meter to change color after certain increments.

--- app.js
+++ app.js
@@ -7,12 +7,22 @@ function plus(){
     if (x >= 10){
         return false;
     }
+    if (x >= 7){
+        meter.style.background = "#f00"
+        meter.style.filter = "drop-shadow(0 0 2.5px #f00) drop-shadow(0 0 10px #f00)"
+    }
     output.innerHTML = ++x;
+    meter.style.height = x*10+'%';
 }
 
 function minus(){
     if (x <= 0){
         return false;
     }
+    if (x <= 7){
+        meter.style.background = "rgb(4,209,255)"
+        meter.style.filter = "drop-shadow(0 0 2.5px rgb(4,209,255)) drop-shadow(0 0 10px rgb(4,209,255))"
+    }
     output.innerHTML = --x;
+    meter.style.height = x*10+'%';
 }
